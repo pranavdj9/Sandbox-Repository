7a5aefc3bbba1f9801d0f0028a3e87e0870ea280
pranavdj9 <68274176+pranavdj9@users.noreply.github.com>
2020-08-08 12:04:17 +0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const img = "https://picsum.photos/200";
+
+ReactDOM.render(
+  <div>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <img src={img} alt="" />
+  </div>,
+  document.getElementById("root")
+);
